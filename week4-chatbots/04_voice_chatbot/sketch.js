b1e3f2049506544e239212aa9e8a015a44d4b3d6
shiffman <daniel@shiffman.net>
2018-09-26 10:52:20 -0400

    example cleanup

--- week4-chatbots/04_voice_chatbot/sketch.js
+++ week4-chatbots/04_voice_chatbot/sketch.js
@@ -1,9 +1,7 @@
 // Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
 
 // Voice Chatbot with p5.Speech
-// Edited Video: https://youtu.be/iFTgphKCP9U
+// Video: https://youtu.be/iFTgphKCP9U
 
 function setup() {
   noCanvas();
@@ -22,7 +20,7 @@ function setup() {
   function gotSpeech() {
     if (speechRec.resultValue) {
       let input = speechRec.resultString;
-      //user_input.value(input);
+      // user_input.value(input);
       bot.reply("local-user", input).then(reply => speech.speak(reply));
     }
   }
