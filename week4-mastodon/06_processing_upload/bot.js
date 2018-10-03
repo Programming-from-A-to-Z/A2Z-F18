7399b2f12f0720d5cefafa250622ca748ee59df8
shiffman <daniel@shiffman.net>
2018-10-03 10:58:03 -0400

    uploading media to mastodon examples

---
+++ week4-mastodon/06_processing_upload/bot.js
@@ -0,0 +1,48 @@
+// A2Z F18
+// Daniel Shiffman
+// http://shiffman.net/a2z
+// https://github.com/shiffman/A2Z-F18
+
+// Using the Mastodon node package
+// https://github.com/vanita5/mastodon-api
+
+const Mastodon = require('mastodon-api');
+const fs = require('fs');
+const exec = require('child_process').exec;
+
+
+// Authorization for the bot
+// https://botsin.space/oauth/authorized_applications
+const config = require('./config.js');
+
+// Making a Twit object for connection to the API
+const M = new Mastodon(config);
+
+// Here is the command to run the Processing sketch
+// You need to have Processing command line installed
+// See: https://github.com/processing/processing/wiki/Command-Line
+var cmd = 'processing-java --sketch=`pwd`/randomwalk/ --run';
+exec(cmd, processing);
+
+// Callback for command line process
+function processing(error, stdout, stderr) {
+  const params = {
+    file: fs.createReadStream('randomwalk/output.png'),
+    description: 'an image of a random walk'
+  }
+
+  M.post('media', params)
+    .then(response => {
+      console.log('image uploaded');
+      const id = response.data.id;
+      const toot = {
+        status: 'random walk',
+        media_ids: [id]
+      };
+      return M.post('statuses', toot)
+    })
+    .then(response => {
+      console.log('Success:' + response.data.content);
+    })
+    .catch(error => console.error(error));
+}
---
+++ week4-mastodon/06_processing_upload/config-empty.js
@@ -0,0 +1,12 @@
+// Change this file to config.js
+// Add your keys
+// Add file .gitignore: config.js
+// Load with 
+// var config = require('./config.js');
+
+module.exports = {
+  client_id: '',
+  client_secret: '',
+  access_token: '',
+  api_url: 'https://botsin.space/api/v1/' // optional, defaults to https://mastodon.social/api/v1/
+}
