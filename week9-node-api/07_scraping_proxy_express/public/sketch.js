f7a8011369c2d45e3267d3a2927af3ed4bd0a79c
shiffman <daniel@shiffman.net>
2018-10-31 11:40:20 -0400

    week 9 examples

---
+++ week9-node-api/07_scraping_proxy_express/public/sketch.js
@@ -0,0 +1,34 @@
+// A2Z F15
+// Daniel Shiffman
+// https://github.com/shiffman/
+
+var url = 'http://www.imdb.com/title/tt0058331/';
+
+function setup() {
+  noCanvas();
+
+  var input = select('#url');
+  var button = select('#submit');
+
+  button.mousePressed(getData);
+
+  function getData() {
+    var url = input.value();
+    loadStrings('load?url=' + url, gotData);
+  }
+}
+
+function gotData(data) {
+  var raw = data.join('\n');
+  console.log(raw);
+
+  const rating = /<span itemprop="ratingValue">(.*?)<\/span>/;
+  var matches = raw.match(rating);
+  createP('rating: ' + matches[1]);
+
+  var poster = /<link rel='image_src' href="(.*?)">/;
+  matches = raw.match(poster);
+  console.log(matches);
+  var img = createImg(matches[1]);
+  img.style('width', '100px');
+}
