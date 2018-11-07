8efa1cc916c6c088c47962d363ad4567a2ec45ed
shiffman <daniel@shiffman.net>
2018-11-07 09:58:06 -0500

    working on sketchRNN example

--- week8-charRNN/04_sketchRNN/sketch.js
+++ week8-charRNN/04_sketchRNN/sketch.js
@@ -1,52 +1,47 @@
-let sketch;
-let prev_pen = [1, 0, 0];
+let model;
+let previous_pen = 'down';
 let x, y;
-
-const options = {
-  temperature: 0.5
-};
+let sketch;
 
 function setup() {
   createCanvas(400, 300);
-  sketch = ml5.SketchRNN('cat', modelReady);
-  background(220);
-
+  model = ml5.SketchRNN('cat', modelReady);
   let button = select('#draw');
-  button.mousePressed(() => {
-    x = width / 2;
-    y = height / 2;
-    background(220);
-    sketch.reset();
-    next();
-  });
+  button.mousePressed(startDrawing);
 }
 
-function next() {
-  sketch
-    .generate(options)
-    .then(response => drawIt(response))
-    .catch(error => console.error(error));
+function startDrawing() {
+  x = width / 2;
+  y = height / 2;
+  background(220);
+  model.reset();
+  model.generate(gotSketch);
+}
+
+function draw() {
+  if (sketch) {
+    if (previous_pen == 'down') {
+      stroke(0);
+      strokeWeight(3.0);
+      line(x, y, x + sketch.dx, y + sketch.dy);
+    }
+    x += sketch.dx;
+    y += sketch.dy;
+    previous_pen = sketch.pen;
+
+    if (sketch.pen !== 'end') {
+      sketch = null;
+      model.generate(gotSketch);
+    }
+  } 
+}
+
+function gotSketch(err, s) {
+  sketch = s;
 }
 
 function modelReady() {
   console.log('model ready');
-  next();
+  startDrawing();
 }
 
-function drawIt(info) {
-  [dx, dy, pen_down, pen_up, pen_end] = info;
-
-  if (prev_pen[0] == 1) {
-    stroke(0);
-    strokeWeight(3.0);
-    line(x, y, x + dx, y + dy);
-  }
-
-  x += dx;
-  y += dy;
-  prev_pen = [pen_down, pen_up, pen_end];
-
-  if (pen_end !== 1) {
-    setTimeout(next, 5);
-  }
-}
