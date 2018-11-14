090425621cf87accd9a41aaf342c8890e9eeaf18
shiffman <daniel@shiffman.net>
2018-11-14 11:13:19 -0500

    moving sentiment examples to week 10

---
+++ week8-charRNN/05_sketchRNN_interactive/sketch.js
@@ -0,0 +1,46 @@
+let model;
+let previous_pen = 'down';
+let x, y;
+let sketch;
+
+function setup() {
+  createCanvas(640, 480);
+  model = ml5.SketchRNN('cat', modelReady);
+  let button = select('#draw');
+  button.mousePressed(startDrawing);
+}
+
+function startDrawing() {
+  x = width / 2;
+  y = height / 2;
+  background(220);
+  model.reset();
+  model.generate(gotSketch);
+}
+
+function draw() {
+  if (sketch) {
+    if (previous_pen == 'down') {
+      stroke(0);
+      strokeWeight(3.0);
+      line(x, y, x + sketch.dx, y + sketch.dy);
+    }
+    x += sketch.dx;
+    y += sketch.dy;
+    previous_pen = sketch.pen;
+
+    if (sketch.pen !== 'end') {
+      sketch = null;
+      model.generate(gotSketch);
+    }
+  }
+}
+
+function gotSketch(err, s) {
+  sketch = s;
+}
+
+function modelReady() {
+  console.log('model ready');
+  startDrawing();
+}
